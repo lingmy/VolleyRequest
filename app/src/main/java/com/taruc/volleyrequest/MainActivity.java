5c60e4c727c6e772df539c6d5c53e7bbda4a0288
taruc <lingmy@tarc.edu.my>
2018-05-24 15:21:09 +0800

    added images request

--- app/src/main/java/com/taruc/volleyrequest/MainActivity.java
+++ app/src/main/java/com/taruc/volleyrequest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.taruc.volleyrequest;
 
+import android.graphics.Bitmap;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,6 +14,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -25,7 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     RequestQueue requestQueue;
 
-    String url ="https://api.myjson.com/bins/n0skq";
+   // String url ="https://api.myjson.com/bins/n0skq";
+   String url ="https://api.myjson.com/bins/1191xm";
+    String imageurl="https://amyling.000webhostapp.com/images/apple.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,18 +59,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try{
                 jobj = new JSONObject(response);
                 String c= jobj.getString("user");
-                    JSONArray jArray = new JSONArray();
+                    JSONArray jArray = new JSONArray(c);
 
                     for (int i = 0; i < jArray.length(); i++) {
                         String id = jArray.getJSONObject(i).getString("id");
                         String name = jArray.getJSONObject(i).getString("name");
                         String age = jArray.getJSONObject(i).getString("age");
                         String married = jArray.getJSONObject(i).getString("married");
+                        textView.append(id+ "   " + name + "    "+age+"    "+married+"\n");
 
                     }
 
             }catch (JSONException e){
                     e.printStackTrace();
+                    Log.d("Main activity",e.getMessage());
             }
             }
         }, new Response.ErrorListener() {
@@ -74,6 +82,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        //image rewuest from 000webhost.com
+        ImageRequest imageRequest = new ImageRequest(imageurl, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+        // image request is successful
+                imageView.setImageBitmap(response);
+            }
+        }, 0, 0, ImageView.ScaleType.CENTER, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+stringRequest.setTag("REQUEST");
+
+
+// add stringRequest into requestQuest
+        requestQueue.add(stringRequest);
+        requestQueue.add(imageRequest);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(requestQueue != null);
+        requestQueue.cancelAll("REQUEST");
 
     }
 }
